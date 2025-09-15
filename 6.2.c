b6c0ac4f3c084671fe2b393842ad466102bcb108
gxy4t6r <dakshgarg7777@gmail.com>
2025-09-15 14:30:02 +0530

    Create 6.2.c

---
+++ 6.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if (num >= 0) {
+        if (num == 0)
+            printf("Number is Zero");
+        else
+            printf("Number is Positive");
+    } else {
+        printf("Number is Negative");
+    }
+
+    return 0;
+}
