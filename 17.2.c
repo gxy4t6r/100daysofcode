157cd8b4d3236756b760d8cc2eca57322a9e050e
gxy4t6r <dakshgarg7777@gmail.com>
2025-09-15 19:59:49 +0530

    Create 17.2.c

---
+++ 17.2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num, i, flag = 0;
+    scanf("%d", &num);
+    if (num <= 1) {
+        printf("Not Prime");
+        return 0;
+    }
+    for (i = 2; i <= num / 2; i++) {
+        if (num % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+        printf("Prime");
+    else
+        printf("Not Prime");
+    return 0;
+}
