bbe3fdf1fcf76ebe9f3c6eba20a60ccd67b09e6a
gxy4t6r <dakshgarg7777@gmail.com>
2025-09-22 11:13:51 +0530

    Create 31.1.c

---
+++ 31.1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,x,f=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++) scanf("%d",&a[i]);
+    scanf("%d",&x);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==x)
+        {
+            f=1;
+            break;
+        }
+    }
+    if(f) printf("Found at index %d",i);
+    else printf("Not Found");
+    return 0;
+}
